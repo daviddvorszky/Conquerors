ec75db3917e6f59f011d1f5cc6ff8dbab526b048
daviddvorszky <dv98david@gmail.com>
2023-11-22 23:24:20 +0100

    Add new and modified routes to the App.

--- client/src/App.js
+++ client/src/App.js
@@ -2,7 +2,8 @@ import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Registration from './components/Auth/Registration';
 import Login from './components/Auth/Login';
-import Profile from './components/Profile';
+import FetchProfile from './components/Profile/FetchProfile';
+import EditPassword from './components/Profile/EditPassword';
 
 const App = () => {
   return (
@@ -10,7 +11,8 @@ const App = () => {
       <Routes>
         <Route path="/register" element={<Registration />} />
         <Route path="/login" element={<Login />} />
-        <Route path="/profile/:username" element={<Profile/>} />
+        <Route path="/profile/:username" element={<FetchProfile/>} />
+        <Route path="/edit-password" element={<EditPassword/>} />
       </Routes>
     </Router>
   );
