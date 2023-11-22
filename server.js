ded82f1ecfebc9f871ea0de08025b26761fad255
daviddvorszky <dv98david@gmail.com>
2023-11-22 16:32:37 +0100

    Add CORS

--- server.js
+++ server.js
@@ -1,9 +1,11 @@
 const express = require('express');
 const app = express();
+const cors = require('cors');
 const userRoutes = require('./routes/user');
 
 require('dotenv').config();
 
+app.use(cors());
 app.use(express.json());
 
 // Use routes
