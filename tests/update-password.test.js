e3be212f4738a58728319b44545ac59a14cc807a
daviddvorszky <dv98david@gmail.com>
2023-11-25 22:13:41 +0100

    Add basic update-password test

---
+++ tests/update-password.test.js
@@ -0,0 +1,41 @@
+const supertest = require('supertest');
+const jwt = require('jsonwebtoken');
+const app = require('..//server');
+const { startTransaction, rollbackTransaction } = require('./dbUtils');
+
+describe('Update-password Endpoint', () => {
+    let client = null;
+    let mockJwtVerify;
+
+    beforeAll(async () => {
+        mockJwtVerify = jest.spyOn(jwt, 'verify').mockImplementation((token, secret, callback) => {
+            callback(null, { user_id: 1, username: 'testuser1' });
+        });
+    });
+
+    afterAll(async () => {
+        mockJwtVerify.mockRestore();
+    });
+
+    beforeEach(async () => {
+        client = await startTransaction();
+        //mockAuthenticateToken();
+    });
+
+    afterEach(async () => {
+        await rollbackTransaction(client);
+    });
+
+    // Test for successful update
+    test('should update password successfully', async () => {
+        const response = await supertest(app)
+            .put('/api/users/update-password')
+            .set('Authorization', 'Bearer fakeToken')
+            .send({
+                newPassword: 'updatedPassword'
+            });
+
+        expect(response.statusCode).toBe(200);
+        expect(response.body).toHaveProperty('message', 'Password updated successfully');
+    });
+});
