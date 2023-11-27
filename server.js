69c69bef76f7676065c9a33b5e10e3c5566d25ed
daviddvorszky <dv98david@gmail.com>
2023-11-27 16:16:48 +0100

    Make command order more logical

--- server.js
+++ server.js
@@ -1,20 +1,18 @@
 const express = require('express');
-const app = express();
 const cors = require('cors');
 const userRoutes = require('./routes/user');
 const matchmakingRoutes = require('./routes/matchmaking');
-
 require('dotenv').config();
 
+const app = express();
 app.use(cors());
 app.use(express.json());
 
-// Use routes
 app.use('/api/users', userRoutes);
 app.use('/api/matchmaking', matchmakingRoutes);
 
 app.get('/', (req, res) => {
-	res.send('Hello World!');
+  res.send('Hello World!');
 });
 
 module.exports = app;
